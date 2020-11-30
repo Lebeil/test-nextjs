8b69d415ad1b1a13b9a0ad0a68ff612f051d273c
Lebeil <liedel75@gmail.com>
2020-11-30 23:17:09 +0100

    module style CSS

--- pages/departements.js
+++ pages/departements.js
@@ -3,15 +3,19 @@ import Layout from "../components/layout";
 import axios from 'axios';
 import {useRouter} from "next/router";
 import Head from "next/head";
+import StyleDepartement from '../styles/Departement.module.css'
 
 const Departement = ({data}) => {
     const router = useRouter()
     console.log(router)
 
     const styles = {
-        padding: 10,
-        margin: 10,
-        borderBottom: "1px solid black"
+        cont: {
+            padding: 20,
+            margin: 20,
+            borderBottom: "3px solid black"
+        }
+
     }
 
     return (
@@ -21,8 +25,8 @@ const Departement = ({data}) => {
             </Head>
             <Layout>
                 {data.map(departement=> (
-                    <div style={styles} key={departement.code}>
-                        <h1>{departement.nom}</h1>
+                    <div className={StyleDepartement.header} key={departement.code}>
+                        <h1 className={StyleDepartement.h1}>{departement.nom}</h1>
                         <div>code de département : {departement.code}</div>
                         <div>Code de la région: {departement.codeRegion}</div>
                     </div>
